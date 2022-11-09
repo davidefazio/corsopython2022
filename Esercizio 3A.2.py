52cb8f2da2e6a355c6f63f67bfca79e7a65ead91
Davide Fazio <fazio@olomedia.it>
2022-11-09 15:23:10 +0100

    Completato l'esercizio (nel precedente avevo frainteso il punto D). Aggiunto un punto D1 che era quello che avevo pensato chiedesse il punto D

--- Esercizio 3A.2.py
+++ Esercizio 3A.2.py
@@ -8,8 +8,9 @@
 # Scrivere un programma Python che:
 # A. Richiede all’utente di inserire un numero N compreso tra 5 e 15
 # B. Richiede all’utente di inserire N elementi e li memorizza in una lista L
-# C. Creare una copia L2 della lista L
-# D. Modificare il valore di N/3 elementi della lista L2 scelti in modo casuale
+# C. Creare una copia L2 e L3 della lista L
+# D1. Modificare il valore dell'elemento in posizione della lista L2 con valori casuali tra 0 e 100
+# D. Modificare il valore di N/3 elementi della lista L3 scelti in modo casuale
 # con valori casuali tra 0 e 100 (valutare se opportuno scrivere una
 # funzione)
 # E. Stampare (utilizzare la funzione sommaElem(list) dell’esercizio 1) il
@@ -26,6 +27,15 @@ def somma_elem(lista):
     return somma
 
 
+def modifica_elem(lista, n_elem_to_modify, min_new_value=0, max_new_value=100):
+    pos_modified = list()
+    for i in range(0, n_elem_to_modify):
+        pos_to_modify = randrange(0, len(lista))
+        if not (pos_to_modify in pos_modified):
+            lista[pos_to_modify] = randrange(min_new_value, max_new_value + 1)
+    return lista
+
+
 n = 0
 while n < 5 or n > 15:
     n = int(input('Inserisci un numero intero compreso tra 5 e 15: '))
@@ -37,11 +47,16 @@ for e in range(0, n):
     lista1.append(elem)
 
 lista2 = lista1.copy()
+lista3 = lista1.copy()
 pos = n // 3
+n_elem = pos
 lista2[pos] = randrange(0, 101)
+lista3 = modifica_elem(lista3, n_elem)
 
 somma1 = somma_elem(lista1)
 somma2 = somma_elem(lista2)
+somma3 = somma_elem(lista3)
 
 print('La somma della lista 1', lista1, 'è', somma1)
 print('La somma della lista 2', lista2, 'è', somma2)
+print('La somma della lista 3', lista3, 'è', somma3)
